4fcac2688554bd3b09436391fd2524de877569a4
yuto0503 <y.yuto.0503@gmail.com>
2026-07-18 16:44:29 +0900

    フロントの一時的に出てしまう500エラーを修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
 export default async function Home() {
-  const response = await fetch(`${process.env.SERVER_API_BASE_URL}/health`);
-  const data = await response.json();
+  try {
+    const response = await fetch(`${process.env.SERVER_API_BASE_URL}/health`);
 
-  console.log(data);
+    if (!response.ok) {
+      throw new Error("API Error");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 
   return <div>通信成功</div>;
 }
